30803915e60ef11a1b41d931a28e1992bb3667b7
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-07-05 01:39:18 +0200

    feat(agent): Including wrapper

---
+++ agent/agent/src/main.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from http import HTTPStatus
+
+from elara_wrapper import add_elara_middleware
+from fastapi import FastAPI, HTTPException
+
+from agent.crew import AgentCrew
+
+app = FastAPI()
+
+# Add the Elara middleware with a validation string
+middleware = add_elara_middleware(app, "True")
+
+
+@app.post("/generate")
+async def root():
+
+    inputs = {
+        'topic': 'AI LLMs',
+        'current_year': str(datetime.now().year)
+    }
+
+    try:
+        output = AgentCrew().crew().kickoff(inputs=inputs)
+        return {"message": "Crew executed successfully", "output": output.raw}
+    except Exception as e:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"An error occurred while running the crew: {e}")
+
